6f7f18c64b01a4009f9dc9aa0ac37fb3b523f464
Jade Levesque <jadedml@openmailbox.org>
2017-05-26 02:18:52 -0500

    Completed manpage rebranding

--- man_deblob.sh
+++ man_deblob.sh
@@ -31,6 +31,13 @@ else
         SRC_DIR=$1
 fi
 
+arch_list="amd64 i386"
+
+for arch in $arg_list
+do
+	linedel "./usr/share/man/man1/fw_update.1" "distrib/sets/lists/base/md.${arch}"
+done
+
 
 # man4
 fw_list="acx adw adv athn bnx bwi drm fxp inteldrm ips ipw iwi iwm iwn kue malo myx neo otus pgt ral"
@@ -41,6 +48,14 @@ do
         strdel "\^${man_blob}.4" share/man/man4/Makefile
         linedel "${man_blob}.4" distrib/sets/lists/man/mi
 done
-linedel "MLINKS+=adv.4 adw.4" share/man/man4/Makefile
-linedel "MLINKS+=drm.4 inteldrm.4 drm.4 radeondrm.4" share/man/man4/Makefile
-linedel "MLINKS+=tht.4 thtc.4" share/man/man4/Makefile
+
+strdel "acx.4" share/man/man4/Makefile
+strdel "upgt.4" share/man/man4/Makefile
+strdel "yds.4" share/man/man4/Makefile
+rep "ural.4" "TEMP.4" share/man/man4/Makefile   # The above loop doesn't catch these-- but strdel "ral.4"
+strdel "ral.4" share/man/man4/Makefile          # catches ural. So ural is switched out with TEMP beforehand.
+rep "TEMP.4" "ural.4" share/man/man4/Makefile
+
+#linedel "MLINKS+=adv.4 adw.4" share/man/man4/Makefile
+#linedel "MLINKS+=drm.4 inteldrm.4 drm.4 radeondrm.4" share/man/man4/Makefile
+#linedel "MLINKS+=tht.4 thtc.4" share/man/man4/Makefile
--- man_rebrand.sh
+++ man_rebrand.sh
@@ -14,7 +14,7 @@
 
 . ./libdeblob.sh
 
-PATCH_DIR=/tmp/man_deblob
+PATCH_DIR=/tmp/man_rebrand
 
 
 if [ -e $PATCH_DIR ]
@@ -27,7 +27,7 @@ fi
 
 if test -z $1
 then
-        SRC_DIR=/usr/src/share/man
+        SRC_DIR=/usr/src
 else
         SRC_DIR=$1
 fi
@@ -35,24 +35,84 @@ fi
 
 # man1
 rep "administrators with" "administrators with LibertyBSD" share/man/man1/help.1
-linedel "^Other$" "man1/help.1"
+linedel "^Other$" "man1/help.1" share/man/man1/help.1
 linedel "references include the FAQ" share/man/man1/help.1
-linedel "https://openbsd.org/faq"
+linedel "https://openbsd.org/faq" share/man/man1/help.1
 linedel "which is mostly intended for administrators" share/man/man1/help.1
 linedel "a working knowledge of" share/man/man1/help.1
-linedel "\.Ux \." "man1/help.1"
+linedel "\.Ux \." "man1/help.1" share/man/man1/help.1
 linedel "There are also mailing lists in place" share/man/man1/help.1
-rep "\.Ox ," "LibertyBSD," share/man/man1/help.1
-rep "available;" "available; LibertyBSD" share/man/man1/help.1
-linedel "\.Ox" share/man/man1/help.1
+#rep "\.Ox ," "LibertyBSD," share/man/man1/help.1
+#rep "available;" "available; LibertyBSD" share/man/man1/help.1
+#linedel "\.Ox" share/man/man1/help.1
 
-rep "\.Ox" "LibertyBSD" share/man/man1/gcc-local.1
-rep "\.Ox" "LibertyBSD" share/man/man1/clang-local.1
+#rep "\.Ox" "LibertyBSD" share/man/man1/gcc-local.1
+#rep "\.Ox" "LibertyBSD" share/man/man1/clang-local.1
 
 
 # man3
-rep "\.Ox" "LibertyBSD" share/man/man3/makedev.3
-rep "\.Ox" "LibertyBSD" share/man/man3/sysexits.3
+#rep "\.Ox" "LibertyBSD" share/man/man3/makedev.3
+#rep "\.Ox" "LibertyBSD" share/man/man3/sysexits.3
 
 
-# man4
+# man5
+#rep "OpenBSD
+
+
+lbsd_replace() {
+	bnumbers="0 1 2 3 4 5" # X.x
+	snumbers="0 1 2 3 4 5 6 7 8 9" #x.X
+	for bnumber in $bnumbers
+	do
+		for snumber in $snumbers
+		do
+			version="$bnumber.$snumber"
+			if [ "$version" == "5.6" ] || [ "$version" == "5.8" ] || [ "$version" == "5.9" ] || [ "$version" == "6.1" ]
+			then
+				echo "Nenion fari." > /dev/null
+			else
+				echo "LibertyBSD $version"
+				echo "$1"
+				if grep "LibertyBSD $version" $1
+				then
+					rep "LibertyBSD $version " ".Ox $version" "$2"
+					rep "LibertyBSD $version" ".Ox $version" "$2"
+				fi
+			fi
+		done
+	done
+}
+
+
+ox_replace() {
+	for file in ${SRC_DIR}/$1/*
+	do
+		echo $file
+		rfile=$(echo $file | sed 's^'"$SRC_DIR"'^^' | sed 's^/^^')
+		echo $rfile
+		if grep ".Ox \." $file
+		then
+			rep ".Ox \." "LibertyBSD\." "$rfile"
+		fi
+		if grep ".Ox \," $file
+		then
+			rep ".Ox \," "LibertyBSD\," "$rfile"
+		fi
+		if grep ".Ox \:" $file
+		then
+			rep ".Ox \:" "LibertyBSD\:" "$rfile"
+		fi
+		if grep ".Ox$" $file
+		then
+			rep ".Ox$" "LibertyBSD" "$rfile"
+		fi
+	done
+}
+
+mandirectories="man0 man1 man3 man4 man5 man6 man7 man8 man9"
+for mandir in $mandirectories
+do
+	ox_replace "share/man/$mandir"
+done
+
+apply
