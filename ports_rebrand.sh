cdbf2502de43db5675d7a2b52a85fba7164b87a1
Jade Levesque <jadedml@openmailbox.org>
2017-06-12 17:26:20 -0500

    More ports added to lists, general cleanup & simplification

--- ports_rebrand.sh
+++ ports_rebrand.sh
@@ -32,56 +32,53 @@ fi
 
 rep "\${MACHINE-ARCH}-openbsd" "\${MACHINE-ARCH}-libertybsd" infrastructure/mk/perl.port.mk
 
-# The ones that require build_alias
-archiverslist="archivers/libzip archivers/gcab archivers/gcpio archivers/gshar+gunshar archivers/gtar archivers/libarchive archivers/libmspack archivers/libtar archivers/lzo"
+# List of ports that require an OBSD build_alias env var
+archiverslist="archivers/libzip archivers/gcab archivers/gcpio archivers/gshar+gunshar archivers/gtar archivers/libarchive archivers/libmspack archivers/libtar archivers/lzo archivers/xz"
 archiverslist="$archiverslsit archivers/lzop archivers/par2cmdline"
 audiolist="audio/freealut"
-dabataseslist="databases/db/v3 databases/db/v4 databases/openldap"
+converterslist="converters/libiconv"
+dabataseslist="databases/db/v3 databases/db/v4 databases/openldap databases/sqlite3 databases/gdbm"
 devellist="devel/autoconf/2.13 devel/autoconf/2.52 devel/autoconf/2.54 devel/autoconf/2.56 devel/autoconf/2.57 devel/autoconf/2.58 devel/autoconf/2.59 devel/autoconf/2.60"
 devellist="$devellist devel/autoconf/2.61 devel/autoconf/2.62 devel/autoconf/2.63 devel/autoconf/2.64 devel/autoconf/2.65 devel/autoconf/2.66 devel/autoconf/2.67 devel/autoconf/2.68"
 devellist="$devllist devel/autoconf/2.69 devel/automake/1.4 devel/automake/1.8 devel/automake/1.9 devel/automake/1.10 devel/automake/1.11 devel/automake/1.12 devel/automake/1.13"
 devellist="$devellist devel/automake/1.14 devel/automake/1.15 devel/libtool devel/libidn devel/gmake devel/llvm devel/sdl2-image devel/sdl2 devel/apr devel/apr-util devel/t1lib"
-devellist="$devellist devel/bison"
+devellist="$devellist devel/bison devel/gettext devel/gettext-tools devel/libsigsegv devel/ffcall"
 editorslist="editors/nano"
 gameslist="games/xscorch"
 geolist="geo/spatialindex"
-langlist="lang/ghc"
+langlist="lang/ghc lang/clisp"
 maillist="mail/mutt mail/alpine"
 netlist="net/openvpn"
+printlist="print/libpaper"
 securitylist="security/cyrus-sasl2 security/libmcrypt"
+shellslist="shells/bash"
 sysutilslist="sysutils/e2fsprogs"
 textproclist="textproc/groff"
 wwwlist="www/lynx www/apache-httpd"
 
 portlist="$archiverslist $audiolist $databaseslist $devellist $editorslist $gameslist $geolist $langlist $maillist $netlist $securitylist $sysutilslist $textproclist $wwwlist"
 
+# Add the build_alias env variable to listed ports
 for port in $portlist
 do
 	rep ".include <bsd.port.mk>" "CONFIGURE_ENV +=        build_alias=\"\${ARCH}-unknown-openbsd6.1\"" $port/Makefile
 	lineadd  "CONFIGURE_ENV +=        build_alias=\"\${ARCH}-unknown-openbsd6.1\"" ".include <bsd.port.mk>" $port/Makefile
 done
 
+
+# Port-specific changes
 rep ".include <bsd.port.mk>" "CONFIGURE_ENV +=        build_alias=\"\${ARCH}-unknown-openbsd\"" lang/ghc/Makefile
 lineadd  "CONFIGURE_ENV +=        build_alias=\"\${ARCH}-unknown-openbsd\"" ".include <bsd.port.mk>" lang/ghc/Makefile
+lineadd "pre-configure:" "        cp -r \${FILESDIR}/Platform/LibertyBSD.cmake \${WRKSRC}/Modules/Platform/LibertyBSD.cmake" devel/cmake/Makefile
+dircp files/ports/files/Platform devel/cmake/files/Platform
 
 
-devellist="devel/cmake"
-portlist="$audiolist"
-for port in $portlist
-do
-	if grep "pre-configre:" $SRC_DIR/$port/Makefile
-	then
-		lineadd "pre-configure:" "        cp -r \${FILESDIR}/Platform/LibertyBSD.cmake \${WRKSRC}/Modules/Platform/LibertyBSD.cmake" $port/Makefile
-	else
-		rep ".include <bsd.port.mk>" "pre-configure:" $port/Makefile
-		lineadd "pre-configure:" "        cp \${FILESDIR}/Platform/LibertyBSD.cmake \${WRKSRC}/Modules/Platform/LibertyBSD.cmake" $port/Makefile
-		lineadd "        cp \${FILESDIR}/Platform/LibertyBSD.cmake \${WRKSRC}/Modules/Platform/LibertyBSD.cmake" ".include <bsd.port.mk>" $port/Makefile
-	fi
-	dircp files/ports/files/Platform $port/files/Platform
-done
+# Misc. infrastructure edits
+lineadd "*:OpenBSD:*:*)" "*:LibertyBSD:*:*)" infrastructure/db/config.guess
+lineadd "*:OpenBSD:*:*)" "        exit ;;" infrastructure/db/config.guess
+lineadd "*:OpenBSD:*:*)" "        echo \${UNAME_MACHINE_ARCH}-unknown-openbsd\${UNAME_RELEASE}" infrastructure/db/config.guess
+lineadd "*:OpenBSD:*:*)" "        UNAME_MACHINE_ARCH=\`arch | sed 's/^.*BSD\.//'\`" infrastructure/db/config.guess
 
-# Installing files
-dircp files/ports/files/cmake/Platform devel/cmake/files/Platform
-lineadd "pre-configure:" "        cp \${FILESDIR}/Platform/LibertyBSD.cmake \${WRKSRC}/Modules/Platform/LibertyBSD.cmake" devel/cmake/Makefile
-
+# Problem childs:
+# * lang/python/2.7	"configure: error: cannot run C compiled programs."	It configures just fine (using same args as used when configuring in ports) manually, but not using port.
 apply
