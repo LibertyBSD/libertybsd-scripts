416b951bc5ae8cf5596409fcecad44d6840bf5b3
Jade Levesque <jadedml@openmailbox.org>
2017-07-02 07:04:27 -0500

    Updated rebranding of ports

--- ports_rebrand.sh
+++ ports_rebrand.sh
@@ -42,6 +42,7 @@ devellist="$devellist devel/autoconf/2.61 devel/autoconf/2.62 devel/autoconf/2.6
 devellist="$devllist devel/autoconf/2.69 devel/automake/1.4 devel/automake/1.8 devel/automake/1.9 devel/automake/1.10 devel/automake/1.11 devel/automake/1.12 devel/automake/1.13"
 devellist="$devellist devel/automake/1.14 devel/automake/1.15 devel/libtool devel/libidn devel/gmake devel/llvm devel/sdl2-image devel/sdl2 devel/apr devel/apr-util devel/t1lib"
 devellist="$devellist devel/bison devel/gettext devel/gettext-tools devel/libsigsegv devel/ffcall devel/gobject-introspection devel/yasm"
+x11list="x11/gnome/at-spi2-core x11/gnome/at-spi2-atk"
 editorslist="editors/nano"
 gameslist="games/xscorch"
 graphicslist="graphics/cairo"
@@ -50,7 +51,7 @@ langlist="lang/ghc lang/clisp"
 maillist="mail/mutt mail/alpine"
 netlist="net/openvpn"
 printlist="print/libpaper"
-securitylist="security/cyrus-sasl2 security/libmcrypt"
+securitylist="security/cyrus-sasl2 security/libmcrypt security/libtasn1 security/p11-kit"
 shellslist="shells/bash"
 sysutilslist="sysutils/e2fsprogs"
 textproclist="textproc/groff"
@@ -84,6 +85,8 @@ rep ".include <bsd.port.mk>" "CONFIGURE_ENV +=        build_alias=\"\${ARCH}-unk
 lineadd  "CONFIGURE_ENV +=        build_alias=\"\${ARCH}-unknown-openbsd\"" ".include <bsd.port.mk>" lang/ghc/Makefile
 lineadd "pre-configure:" "        @cp \${FILESDIR}/Platform/LibertyBSD.cmake \${WRKSRC}/Modules/Platform/LibertyBSD.cmake" devel/cmake/Makefile
 dircp files/ports/files/cmake/Platform devel/cmake/files/Platform
+lineadd "pre-configure:" "        @cp \${FILESDIR}/platforms/LibertyBSD.cmake \${WRKSRC}/cmake/platforms/LibertyBSD.cmake" devel/llvm/Makefile
+dircp files/ports/files/llvm/ devel/llvm/files/
 
 # *.mk edits
 rep "\${MACHINE-ARCH}-openbsd" "\${MACHINE-ARCH}-libertybsd" infrastructure/mk/perl.port.mk
@@ -98,6 +101,8 @@ lineadd "*:OpenBSD:*:*)" "        UNAME_MACHINE_ARCH=\`arch | sed 's/^.*BSD\.//'
 # * lang/python/2.7	"configure: error: cannot run C compiled programs."	It configures just fine (using same args as used when configuring in ports) manually, but not using port.
 # * lang/tcl
 # * x11/tk
+# * devel/llvm
+# * security/gnutls
 
 # Go through ports with additional patches for LBSD
 for category in files/ports/files/patches/*
@@ -122,6 +127,9 @@ linedel "\$ ftp ftp://ftp.openbsd.org/pub/OpenBSD/snapshots/sparc/cd52.iso" emul
 rep "install52.fs" "install61.fs" emulators/qemu/pkg/README
 rep "install52.iso" "install61.iso" emulators/qemu/pkg/README
 rep "OpenBSD" "LibertyBSD" emulators/qemu/pkg/README
-# @jimmybot for this ^^
+rep "ftp.openbsd.org" "ftp.libertybsd.net" sysutils/ruby-puppet/4/patches/patch-lib_puppet_provider_package-openbsd_rb
+rep "on OpenBSD" "on LibertyBSD" sysutils/sysmon/pkg/README ; rep "openbsd.org" "libertybsd.net" sysutils/sysmon/pkg/README
+rep "=\"OpenBSD " "=\"LibertyBSD " multimedia/gstreamer-0.10/Makefile.inc
+# @jimmybot for this :) ^^
 
 apply
